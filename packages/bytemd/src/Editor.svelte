ed29dca161a2b2cc0721df9e5580751ebee4c3ea
Rongjian Zhang <pd4d10@gmail.com>
2020-03-02 22:02:03 +0800

    feat: using codemirror as editor

--- packages/bytemd/src/Editor.svelte
+++ packages/bytemd/src/Editor.svelte
@@ -1,7 +1,22 @@
 <script>
+  import { onMount } from 'svelte';
+  import codemirror from 'codemirror';
+  import mdMode from 'codemirror/mode/markdown/markdown.js';
   import Viewer from './Viewer.svelte';
+
   window.process = { cwd: () => '/' }; // FIXME:
   export let source;
+  let textarea;
+
+  onMount(() => {
+    const ed = codemirror.fromTextArea(textarea, {
+      mode: 'markdown'
+    });
+    ed.setValue(source);
+    ed.on('change', () => {
+      source = ed.getValue();
+    });
+  });
 </script>
 
 <style>
@@ -9,14 +24,9 @@
     display: flex;
     height: 100vh;
   }
-  textarea {
-    flex: 1;
-    font-size: 14px;
-    padding: 10px;
-  }
 </style>
 
 <div>
-  <textarea cols="30" rows="10" bind:value={source} />
+  <textarea bind:this={textarea} />
   <Viewer {source} />
 </div>
