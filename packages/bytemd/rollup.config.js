f63892151b8d7efb0dcf2dc95915aebb3f7e47b7
Rongjian Zhang <pd4d10@gmail.com>
2020-03-02 19:33:04 +0800

    build: add cjs and esm outputs

--- packages/bytemd/rollup.config.js
+++ packages/bytemd/rollup.config.js
@@ -3,29 +3,46 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
 import builtins from 'rollup-plugin-node-builtins';
-
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 
 const production = !process.env.ROLLUP_WATCH;
 
+const outputConfig = {
+  sourcemap: true,
+  name: 'bytemd'
+};
+
+const pkg = require('./package.json');
+
 export default {
   input: 'src/index.js',
-  output: {
-    sourcemap: true,
-    format: 'iife',
-    name: 'bytemd',
-    file: 'public/build/bundle.js'
-  },
+  output: [
+    {
+      ...outputConfig,
+      format: 'es',
+      file: pkg.module
+    },
+    {
+      ...outputConfig,
+      format: 'cjs',
+      file: pkg.main
+    },
+    {
+      ...outputConfig,
+      format: 'umd',
+      file: 'public/build/bytemd.js'
+    }
+  ],
   plugins: [
     svelte({
       // enable run-time checks when not in production
-      dev: !production,
+      dev: !production
       // we'll extract any component CSS out into
       // a separate file - better for performance
-      css: css => {
-        css.write('public/build/bundle.css');
-      }
+      // css: css => {
+      //   css.write('public/build/bytemd.css');
+      // }
     }),
 
     // If you have external dependencies installed from
