5fbff7b04f39a67580952dd6799eeaac121ba132
Rongjian Zhang <pd4d10@gmail.com>
2020-03-04 18:33:36 +0800

    feat(react): add type definition

---
+++ packages/react/index.d.ts
@@ -0,0 +1,5 @@
+import { Component } from 'react';
+import { ViewerProps, EditorProps } from '@bytemd/core';
+
+export declare class Viewer extends Component<ViewerProps> {}
+export declare class Editor extends Component<EditorProps> {}
