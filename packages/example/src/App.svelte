19cc00013031718d74694f197c519b68ac6b4993
Rongjian Zhang <pd4d10@gmail.com>
2020-03-06 23:36:09 +0800

    chore: fixtures

--- packages/example/src/App.svelte
+++ packages/example/src/App.svelte
@@ -1,12 +1,31 @@
 <script>
-  import { onMount } from 'svelte';
   import { Editor } from 'bytemd';
-  let source = '```javascript\nconst x = 1\n```';
+  let source = `# bytemd
 
-  onMount(async () => {
-    // const res = await fetch('/fixture.md');
-    // source = await res.text();
-  });
+[![npm](https://img.shields.io/npm/v/bytemd.svg)](https://npm.im/bytemd)
+
+## Usage
+
+\`\`\`js
+import { Editor, Viewer } from 'bytemd';
+
+// Editor
+new Editor({
+  target: document.body,
+  props: {}
+});
+
+// Viewer
+new Viewer({
+  target: document.body,
+  props: {}
+});
+\`\`\`
+
+## License
+
+MIT
+`
 </script>
 
 <Editor {source} />
