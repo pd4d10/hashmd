4a86c57f04abcc172dde5a5f52cb096c149e6b8d
Rongjian Zhang <pd4d10@gmail.com>
2020-03-08 16:21:33 +0800

    refactor: rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -12,66 +12,16 @@ import visualizer from 'rollup-plugin-visualizer';
 
 const production = !process.env.ROLLUP_WATCH;
 
-const corePkg = require('./packages/bytemd/package.json');
-const reactPkg = require('./packages/bytemd-react/package.json');
-const highlightPkg = require('./packages/plugin-highlight/package.json');
-const mathPkg = require('./packages/plugin-math/package.json');
-const graphvizPkg = require('./packages/plugin-graphviz/package.json');
-const mermaidPkg = require('./packages/plugin-mermaid/package.json');
-
-function serve() {
-  let started = false;
-
-  return {
-    writeBundle() {
-      if (!started) {
-        started = true;
-
-        require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-          stdio: ['ignore', 'inherit', 'inherit'],
-          shell: true
-        });
-      }
-    }
-  };
-}
-
 /** @type {{ [key: string]: import('rollup').RollupOptions}} */
 const configs = {
   bytemd: {
     input: 'src/index.js',
-    output: [
-      {
-        format: 'es',
-        file: corePkg.module
-      },
-      {
-        format: 'cjs',
-        file: corePkg.main
-      },
-      {
-        format: 'umd',
-        name: 'bytemd',
-        file: corePkg.unpkg
-      }
-    ],
     watch: {
       clearScreen: false
     }
   },
   'bytemd-react': {
-    input: 'src/index.js',
-    external: ['bytemd', 'react'],
-    output: [
-      {
-        format: 'es',
-        file: reactPkg.module
-      },
-      {
-        format: 'cjs',
-        file: reactPkg.main
-      }
-    ]
+    external: ['bytemd', 'react']
   },
   example: {
     input: 'src/main.js',
@@ -83,62 +33,30 @@ const configs = {
       }
     ]
   },
-  'plugin-highlight': {
-    input: 'src/index.js',
-    output: [
-      {
-        format: 'es',
-        file: highlightPkg.module
-      },
-      {
-        format: 'cjs',
-        file: highlightPkg.main
-      }
-    ]
-  },
-  'plugin-math': {
-    input: 'src/index.js',
-    output: [
-      {
-        format: 'es',
-        file: mathPkg.module
-      },
-      {
-        format: 'cjs',
-        file: mathPkg.main
-      }
-    ]
-  },
-  'plugin-graphviz': {
-    input: 'src/index.js',
-    output: [
-      {
-        format: 'es',
-        file: graphvizPkg.module
-      },
-      {
-        format: 'cjs',
-        file: graphvizPkg.main
-      }
-    ]
-  },
-  'plugin-mermaid': {
-    input: 'src/index.js',
-    output: [
-      {
-        format: 'es',
-        file: mermaidPkg.module
-      },
-      {
-        format: 'cjs',
-        file: mermaidPkg.main
-      }
-    ]
-  }
+  'plugin-highlight': {},
+  'plugin-math': {},
+  'plugin-graphviz': {},
+  'plugin-mermaid': {}
 };
 
 Object.entries(configs).forEach(([k, v]) => {
+  if (!v.input) {
+    v.input = 'src/index.js';
+  }
   v.input = path.resolve('packages', k, v.input);
+  if (!v.output) {
+    const pkg = require(`./packages/${k}/package.json`);
+    v.output = [
+      {
+        format: 'es',
+        file: pkg.module
+      },
+      {
+        format: 'cjs',
+        file: pkg.main
+      }
+    ];
+  }
   v.output.forEach(output => {
     output.file = path.resolve('packages', k, output.file);
     output.sourcemap = true;
