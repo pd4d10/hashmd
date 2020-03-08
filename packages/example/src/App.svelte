ec43922f262b42e5ab5621a1b1556e939dc83958
Rongjian Zhang <pd4d10@gmail.com>
2020-03-08 14:30:09 +0800

    chore: add mermaid example

--- packages/example/src/App.svelte
+++ packages/example/src/App.svelte
@@ -3,15 +3,27 @@
   import highlight from '@bytemd/plugin-highlight'
   import math from '@bytemd/plugin-math'
   import graphviz from '@bytemd/plugin-graphviz'
+  import mermaid from '@bytemd/plugin-mermaid'
   let source = `# bytemd
 
 [![npm](https://img.shields.io/npm/v/bytemd.svg)](https://npm.im/bytemd)
+
+## Math equation
 
 $$
 c = \\pm\\sqrt{a^2 + b^2}
 $$
 
-## Usage
+## Mermaid
+
+\`\`\`mermaid
+sequenceDiagram
+    Alice->>John: Hello John, how are you?
+    %% this is a comment
+    John-->>Alice: Great!
+\`\`\`
+
+## Code
 
 \`\`\`js
 import { Editor, Viewer } from 'bytemd';
@@ -28,10 +40,6 @@ new Viewer({
   props: {}
 });
 \`\`\`
-
-## License
-
-MIT
 `
 </script>
 
