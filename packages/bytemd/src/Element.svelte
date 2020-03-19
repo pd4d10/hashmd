6123d9a057c422dafd52602a070c5393ca7e7d33
Rongjian Zhang <pd4d10@gmail.com>
2020-03-19 15:51:01 +0800

    refactor: render

--- packages/bytemd/src/Element.svelte
+++ packages/bytemd/src/Element.svelte
@@ -4,73 +4,78 @@
 
   export let node;
   export let plugins;
+
+  $: type = node.type
+  $: value = node.value
+  $: tagName = node.tagName
+  $: children = node.children
+  $: properties = node.properties
 </script>
 
-{#if node.type === 'text'}
-  {node.value}
+{#if type === 'text'}
+  {value}
+{:else if tagName === 'em'}
+  <em><Elements nodes={children} {plugins} /></em>
+{:else if tagName === 'strong'}
+  <strong><Elements nodes={children} {plugins} /></strong>
+{:else if tagName === 'del'}
+  <del><Elements nodes={children} {plugins} /></del>
+{:else if tagName === 'ins'}
+  <ins><Elements nodes={children} {plugins} /></ins>
+{:else if tagName === 'mark'}
+  <mark><Elements nodes={children} {plugins} /></mark>
+{:else if tagName === 'sub'}
+  <sub><Elements nodes={children} {plugins} /></sub>
+{:else if tagName === 'sup'}
+  <sup><Elements nodes={children} {plugins} /></sup>
+{:else if tagName === 'ruby'}
+  <ruby><Elements nodes={children} {plugins} /></ruby>
+{:else if tagName === 'rp'}
+  <rp><Elements nodes={children} {plugins} /></rp>
+{:else if tagName === 'rt'}
+  <rt><Elements nodes={children} {plugins} /></rt>
 
-{:else if node.tagName === 'em'}
-  <em><Elements nodes={node.children} {plugins} /></em>
-{:else if node.tagName === 'strong'}
-  <strong><Elements nodes={node.children} {plugins} /></strong>
-{:else if node.tagName === 'del'}
-  <del><Elements nodes={node.children} {plugins} /></del>
-{:else if node.tagName === 'ins'}
-  <ins><Elements nodes={node.children} {plugins} /></ins>
-{:else if node.tagName === 'mark'}
-  <mark><Elements nodes={node.children} {plugins} /></mark>
-{:else if node.tagName === 'sub'}
-  <sub><Elements nodes={node.children} {plugins} /></sub>
-{:else if node.tagName === 'sup'}
-  <sup><Elements nodes={node.children} {plugins} /></sup>
-{:else if node.tagName === 'ruby'}
-  <ruby><Elements nodes={node.children} {plugins} /></ruby>
-{:else if node.tagName === 'rp'}
-  <rp><Elements nodes={node.children} {plugins} /></rp>
-{:else if node.tagName === 'rt'}
-  <rt><Elements nodes={node.children} {plugins} /></rt>
+{:else if tagName === 'code'}
+  <code><Elements nodes={children} {plugins} /></code>
+{:else if tagName === 'h1'}
+  <h1><Elements nodes={children} {plugins} /></h1>
+{:else if tagName === 'h2'}
+  <h2><Elements nodes={children} {plugins} /></h2>
+{:else if tagName === 'h3'}
+  <h3><Elements nodes={children} {plugins} /></h3>
+{:else if tagName === 'h4'}
+  <h4><Elements nodes={children} {plugins} /></h4>
+{:else if tagName === 'h5'}
+  <h5><Elements nodes={children} {plugins} /></h5>
+{:else if tagName === 'h6'}
+  <h6><Elements nodes={children} {plugins} /></h6>
+{:else if tagName === 'p'}
+  <p align={properties.align}><Elements nodes={children} {plugins} /></p>
+{:else if tagName === 'blockquote'}
+  <blockquote><Elements nodes={children} {plugins} /></blockquote>
+{:else if tagName === 'pre'}
+  <pre><Elements nodes={children} {plugins} /></pre>
+{:else if tagName === 'ol'}
+  <ol><Elements nodes={children} {plugins} /></ol>
+{:else if tagName === 'ul'}
+  <ul><Elements nodes={children} {plugins} /></ul>
+{:else if tagName === 'li'}
+  <li><Elements nodes={children} {plugins} /></li>
+{:else if tagName === 'a'}
+  <a href={santitizeHref(properties.href)}><Elements nodes={children} {plugins} /></a>
+{:else if tagName === 'img'}
+  <img src={properties.src} alt={properties.alt} width={properties.width} height={properties.height} />
 
-{:else if node.tagName === 'code'}
-  <code><Elements nodes={node.children} {plugins} /></code>
-{:else if node.tagName === 'h1'}
-  <h1><Elements nodes={node.children} {plugins} /></h1>
-{:else if node.tagName === 'h2'}
-  <h2><Elements nodes={node.children} {plugins} /></h2>
-{:else if node.tagName === 'h3'}
-  <h3><Elements nodes={node.children} {plugins} /></h3>
-{:else if node.tagName === 'h4'}
-  <h4><Elements nodes={node.children} {plugins} /></h4>
-{:else if node.tagName === 'h5'}
-  <h5><Elements nodes={node.children} {plugins} /></h5>
-{:else if node.tagName === 'h6'}
-  <h6><Elements nodes={node.children} {plugins} /></h6>
-{:else if node.tagName === 'p'}
-  <p align={node.properties.align}><Elements nodes={node.children} {plugins} /></p>
-{:else if node.tagName === 'blockquote'}
-  <blockquote><Elements nodes={node.children} {plugins} /></blockquote>
-{:else if node.tagName === 'pre'}
-  <pre><Elements nodes={node.children} {plugins} /></pre>
-{:else if node.tagName === 'ol'}
-  <ol><Elements nodes={node.children} {plugins} /></ol>
-{:else if node.tagName === 'ul'}
-  <ul><Elements nodes={node.children} {plugins} /></ul>
-{:else if node.tagName === 'li'}
-  <li><Elements nodes={node.children} {plugins} /></li>
-{:else if node.tagName === 'a'}
-  <a href={santitizeHref(node.properties.href)}><Elements nodes={node.children} {plugins} /></a>
-{:else if node.tagName === 'img'}
-  <img src={node.properties.src} alt={node.properties.alt} width={node.properties.width} height={node.properties.height} />
-
-{:else if node.tagName === 'table'}
-  <table><Elements nodes={node.children} {plugins} /></table>
-{:else if node.tagName === 'thead'}
-  <thead><Elements nodes={node.children} {plugins} /></thead>
-{:else if node.tagName === 'tbody'}
-  <tbody><Elements nodes={node.children} {plugins} /></tbody>
-{:else if node.tagName === 'tr'}
-  <tr><Elements nodes={node.children} {plugins} /></tr>
-{:else if node.tagName === 'th'}
-  <th align={node.properties.align}><Elements nodes={node.children} {plugins} /></th>
-{:else if node.tagName === 'td'}
-  <td align={node.properties.align}><Elements nodes={node.children} {plugins} /></td>
+{:else if tagName === 'table'}
+  <table><Elements nodes={children} {plugins} /></table>
+{:else if tagName === 'thead'}
+  <thead><Elements nodes={children} {plugins} /></thead>
+{:else if tagName === 'tbody'}
+  <tbody><Elements nodes={children} {plugins} /></tbody>
+{:else if tagName === 'tr'}
+  <tr><Elements nodes={children} {plugins} /></tr>
+{:else if tagName === 'th'}
+  <th align={properties.align}><Elements nodes={children} {plugins} /></th>
+{:else if tagName === 'td'}
+  <td align={properties.align}><Elements nodes={children} {plugins} /></td>
 {/if}
