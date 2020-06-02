dc088b0eecbdf587d742e92d9a9c9410dbabc9a4
Rongjian Zhang <pd4d10@gmail.com>
2020-06-02 12:01:23 +0800

    fix: plugin render

--- packages/bytemd/src/utils/element.ts
+++ packages/bytemd/src/utils/element.ts
@@ -1,11 +1,12 @@
 import { BytemdPlugin } from 'bytemd';
-import { HastNode } from '../../helpers';
+import { HastNode } from '../helpers';
 
 export function findPlugin(node: HastNode, plugins: BytemdPlugin[]) {
   for (let i = 0; i < plugins.length; i++) {
     const { renderNode } = plugins[i];
     if (renderNode) {
-      return renderNode(node);
+      const res = renderNode(node);
+      if (res) return res;
     }
   }
 }
