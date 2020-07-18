fb27d911fe8a274b1b63369d9482c87af9af2b0e
Rongjian Zhang <pd4d10@gmail.com>
2020-07-18 22:10:09 +0800

    chore: style tweaks

--- packages/bytemd/src/toolbar-button.svelte
+++ packages/bytemd/src/toolbar-button.svelte
@@ -19,24 +19,6 @@
   export let tooltip;
 </script>
 
-<style>
-  span {
-    cursor: pointer;
-    border: 1px solid transparent;
-    margin-left: 6px;
-  }
-  span:hover :global(svg) {
-    fill: #0366d6;
-  }
-  span :global(svg) {
-    display: block;
-    width: 16px;
-    height: 16px;
-    padding: 4px;
-    fill: #586069;
-  }
-</style>
-
 <span bind:this={el} on:click={handleClick}>
   <slot />
 </span>
