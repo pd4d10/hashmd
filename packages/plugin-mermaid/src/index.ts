21b09f4852ddf851e14d959e21cbf37be5c0bfef
Kefah T. Issa <kefah.issa@gmail.com>
2020-09-02 13:31:03 +0300

    fix: Randomize the id of mermaid div (#5)
    * Randomize the id of mermaid div
    
    This is a fix for issue #4
    
    * Update index.ts

--- packages/plugin-mermaid/src/index.ts
+++ packages/plugin-mermaid/src/index.ts
@@ -23,7 +23,7 @@ export default function mermaid(options?: mermaidAPI.Config): BytemdPlugin {
           try {
             const pre = el.parentElement!;
             m.render(
-              `bytemd-mermaid-${i}`,
+              `bytemd-mermaid-${Date.now()}-${i}`,
               el.innerText,
               (svgCode) => {
                 pre.innerHTML = svgCode;
