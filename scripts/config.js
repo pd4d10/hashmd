2bcddcde007b080caa8d7456d73b793fd9f6f20f
Rongjian Zhang <pd4d10@gmail.com>
2020-09-28 19:20:59 +0800

    build: move react and vue wrappers

--- scripts/config.js
+++ scripts/config.js
@@ -1,8 +1,20 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs-extra';
+import path from 'path';
 
 const root = path.join(__dirname, '../packages');
 const packages = fs.readdirSync(root);
+
+// tsconfig root
+fs.writeJsonSync(
+  path.resolve(__dirname, '../tsconfig.json'),
+  {
+    files: [],
+    references: packages.map((p) => {
+      return { path: 'packages/' + p };
+    }),
+  },
+  { spaces: 2 }
+);
 
 packages.forEach((p) => {
   // tsconfig
@@ -15,19 +27,18 @@ packages.forEach((p) => {
       outDir: 'lib',
     },
   };
-  if (p.startsWith('plugin-')) {
+  if (p !== 'bytemd') {
     tsconfig.references = [{ path: '../bytemd' }];
   }
 
-  fs.writeFileSync(
-    path.join(root, p, 'tsconfig.json'),
-    JSON.stringify(tsconfig, null, 2) + '\n'
-  );
+  fs.writeJsonSync(path.join(root, p, 'tsconfig.json'), tsconfig, {
+    spaces: 2,
+  });
 
   // package.json
   const pkgPath = path.join(root, p, 'package.json');
   const pkg = require(pkgPath);
   pkg.types = 'lib/index.d.ts';
   pkg.files = ['dist', 'lib'];
-  fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + '\n');
+  fs.writeJsonSync(pkgPath, pkg, { spaces: 2 });
 });
