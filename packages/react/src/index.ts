c7bdbc96427f1d9b50bc127d2c8754f4b7385999
Rongjian Zhang <pd4d10@gmail.com>
2020-10-16 15:55:34 +0800

    Revert "feat: reexport all for wrappers"
    This reverts commit e77898eb59db7bff13854b311f748d453e53a415.

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,4 +1,2 @@
-export * from 'bytemd';
-
 export { Editor, EditorProps } from './editor';
 export { Viewer, ViewerProps } from './viewer';
--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,5 +1,4 @@
 import Viewer from './viewer.vue';
 import Editor from './editor.vue';
 
-export * from 'bytemd';
 export { Editor, Viewer };
