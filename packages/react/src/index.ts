e77898eb59db7bff13854b311f748d453e53a415
Rongjian Zhang <pd4d10@gmail.com>
2020-10-16 14:34:51 +0800

    feat: reexport all for wrappers

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,2 +1,4 @@
+export * from 'bytemd';
+
 export { Editor, EditorProps } from './editor';
 export { Viewer, ViewerProps } from './viewer';
--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,4 +1,5 @@
 import Viewer from './viewer.vue';
 import Editor from './editor.vue';
 
+export * from 'bytemd';
 export { Editor, Viewer };
