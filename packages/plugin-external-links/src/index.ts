a7686b3999d8c25739c5ee0dc8ca4b611fca6992
Rongjian Zhang <pd4d10@gmail.com>
2020-10-18 20:10:16 +0800

    refactor(external-links): more friendly api

--- packages/plugin-external-links/src/index.ts
+++ packages/plugin-external-links/src/index.ts
@@ -8,30 +8,36 @@ interface AnchorProps {
 }
 
 export interface ExternalLinksOptions {
-  props?: AnchorProps | ((href: string) => AnchorProps);
+  /**
+   * Test if it is an external url
+   */
+  test(href: string): boolean;
+  internalProps?: AnchorProps;
+  externalProps?: AnchorProps;
 }
 
 export default function externalLinks({
-  props = {
+  test,
+  internalProps = {},
+  externalProps = {
     target: '_blank',
     rel: 'nofollow noopener noreferrer',
   },
 }: ExternalLinksOptions): BytemdPlugin {
   return {
     rehype: (p) =>
-      p.use(() => {
-        return (tree) => {
-          visit<Element>(tree, 'element', (node) => {
-            if (node.tagName === 'a' && node.properties?.href) {
-              Object.assign(
-                node.properties,
-                typeof props === 'function'
-                  ? props(node.properties.href as string)
-                  : props
-              );
-            }
-          });
-        };
+      p.use(() => (tree) => {
+        visit<Element>(tree, 'element', (node) => {
+          if (node.tagName !== 'a' || !node.properties?.href) return;
+
+          const href = node.properties.href as string;
+          if (!/https?:\/\//.test(href)) return; // only handle http and https
+
+          Object.assign(
+            node.properties,
+            test(href) ? externalProps : internalProps
+          );
+        });
       }),
   };
 }
