cf054644f36f723d4c8d1ff14d8e7bdb4846d0b8
Rongjian Zhang <pd4d10@gmail.com>
2021-03-01 18:17:06 +0800

    feat(mermaid): add dropdown and examples

--- packages/plugin-mermaid/src/index.ts
+++ packages/plugin-mermaid/src/index.ts
@@ -56,14 +56,103 @@ export default function mermaid({
     },
     action: {
       icon: icons.mermaid,
-      handler({ editor, appendBlock }) {
-        const { line } = appendBlock('```mermaid\ngraph LR\nA--->B\n```');
-        editor.setSelection(
-          { line: line + 1, ch: 0 },
-          { line: line + 2, ch: Infinity }
-        );
-        editor.focus();
-      },
+      children: [
+        {
+          title: 'Flowchart',
+          code: `graph TD
+Start --> Stop`,
+        },
+        {
+          title: 'Sequence diagram',
+          code: `sequenceDiagram
+Alice->>John: Hello John, how are you?
+John-->>Alice: Great!
+Alice-)John: See you later!`,
+        },
+        {
+          title: 'Class diagrams',
+          code: `classDiagram
+Animal <|-- Duck
+Animal <|-- Fish
+Animal <|-- Zebra
+Animal : +int age
+Animal : +String gender
+Animal: +isMammal()
+Animal: +mate()
+class Duck{
+    +String beakColor
+    +swim()
+    +quack()
+}
+class Fish{
+    -int sizeInFeet
+    -canEat()
+}
+class Zebra{
+    +bool is_wild
+    +run()
+}`,
+        },
+        {
+          title: 'State diagram',
+          code: `stateDiagram-v2
+[*] --> Still
+Still --> [*]
+
+Still --> Moving
+Moving --> Still
+Moving --> Crash
+Crash --> [*]`,
+        },
+        {
+          title: 'Entity relationship diagram',
+          code: `erDiagram
+CUSTOMER ||--o{ ORDER : places
+ORDER ||--|{ LINE-ITEM : contains
+CUSTOMER }|..|{ DELIVERY-ADDRESS : uses`,
+        },
+        {
+          title: 'User journey diagram',
+          code: `journey
+title My working day
+section Go to work
+  Make tea: 5: Me
+  Go upstairs: 3: Me
+  Do work: 1: Me, Cat
+section Go home
+  Go downstairs: 5: Me
+  Sit down: 5: Me`,
+        },
+        {
+          title: 'Gantt chart',
+          code: `gantt
+title A Gantt Diagram
+dateFormat  YYYY-MM-DD
+section Section
+A task           :a1, 2014-01-01, 30d
+Another task     :after a1  , 20d
+section Another
+Task in sec      :2014-01-12  , 12d
+another task      : 24d`,
+        },
+        {
+          title: 'Pie chart',
+          code: `pie title Pets adopted by volunteers
+"Dogs" : 386
+"Cats" : 85
+"Rats" : 15`,
+        },
+      ].map(({ title, code }) => ({
+        title,
+        handler({ editor, appendBlock }) {
+          const { line } = appendBlock('```mermaid\n' + code + '\n```');
+          editor.setSelection(
+            { line: line + 1, ch: 0 },
+            { line: line + code.split('\n').length, ch: Infinity }
+          );
+          editor.focus();
+        },
+      })),
       ...locale,
     },
   };
