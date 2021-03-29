99100fbdf7dcfbf4af1d2e49f953bcd1012501cb
Rongjian Zhang <pd4d10@gmail.com>
2021-03-29 13:48:06 +0800

    chore: add svelte-kit example

---
+++ examples/svelte-kit/src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import App from '../../../svelte/src/App.svelte';
+</script>
+
+<App />
+
+<style lang="scss">
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+</style>
