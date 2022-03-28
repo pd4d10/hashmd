b7a8a4ca1ceebba9afa0ab73c2f8dcfa7c0cacd8
Rongjian Zhang <pd4d10@gmail.com>
2022-03-28 21:20:30 +0800

    style: format

--- examples/vue-nuxtjs/nuxt.config.js
+++ examples/vue-nuxtjs/nuxt.config.js
@@ -3,26 +3,22 @@ export default {
   head: {
     title: 'vue-nuxtjs',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -34,10 +30,8 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {},
 }
--- examples/vue-nuxtjs/store/README.md
+++ examples/vue-nuxtjs/store/README.md
@@ -2,8 +2,7 @@
 
 **This directory is not required, you can delete it if you don't want to use it.**
 
-This directory contains your Vuex Store files.
-Vuex Store option is implemented in the Nuxt.js framework.
+This directory contains your Vuex Store files. Vuex Store option is implemented in the Nuxt.js framework.
 
 Creating a file in this directory automatically activates the option in the framework.
 
