8de10c21f67758102874bec8efb01f3de9fd3ef5
Rongjian Zhang <pd4d10@gmail.com>
2022-03-29 15:54:47 +0800

    build: upgrade norm

--- scripts/postinstall.mjs
+++ scripts/postinstall.mjs
@@ -124,10 +124,3 @@ ${content}
 )
 
 fs.writeFileSync(path.join(rootDir, 'README.md'), readme)
-
-// format
-execSync('npm run lint:fix', { stdio: 'inherit' })
-execSync('npx sort-package-json package.json packages/*/package.json', {
-  stdio: 'inherit',
-})
-execSync('npx sort-json packages/*/locales/*.json', { stdio: 'inherit' })
