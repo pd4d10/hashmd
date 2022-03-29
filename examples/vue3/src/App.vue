503b8010c7bd8b19093da26b044aac3fedc8217a
Rongjian Zhang <pd4d10@gmail.com>
2022-03-29 23:15:28 +0800

    chore: add vue3 example

---
+++ examples/vue3/src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { Editor } from '@bytemd/vue-next'
+import gfm from '@bytemd/plugin-gfm'
+import 'bytemd/dist/index.css'
+
+let content = ref('')
+
+const plugins = [gfm()]
+
+const handleChange = (v: string) => {
+  content.value = v
+}
+</script>
+
+<template>
+  <main>
+    <Editor :value="content" :plugins="plugins" @change="handleChange" />
+  </main>
+</template>
+
+<style>
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+  font-weight: normal;
+}
+</style>
