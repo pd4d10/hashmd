8b38257e23f3a7cdcb6b6a4c8a5fe008a1fe82da
Rongjian Zhang <pd4d10@gmail.com>
2022-04-05 16:38:41 +0800

    docs: build

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: 'ByteMD',
+  description: 'A hackable Markdown editor component',
+  themeConfig: {
+    repo: 'bytedance/bytemd',
+  },
+})
