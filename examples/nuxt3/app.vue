6d855d2a53fd196325d885a7e544e33ca35443a8
Robert Soriano <sorianorobertc@gmail.com>
2022-06-27 23:18:08 -0700

    docs: add nuxt3 example (#201)

---
+++ examples/nuxt3/app.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Editor } from '@bytemd/vue-next'
+import gfm from '@bytemd/plugin-gfm'
+
+const content = ref('')
+const plugins = [gfm()]
+const handleChange = (v: string) => {
+  content.value = v
+}
+</script>
+
+<template>
+  <ClientOnly>
+    <Editor :value="content" :plugins="plugins" @change="handleChange" />
+  </ClientOnly>
+</template>
---
+++ examples/nuxt3/nuxt.config.ts
@@ -0,0 +1,6 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  css: ['bytemd/dist/index.css']
+})
