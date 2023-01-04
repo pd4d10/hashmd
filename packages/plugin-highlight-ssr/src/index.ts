24d90909a8e8134dd85144319c9ab963512ace1d
Rongjian Zhang <pd4d10@gmail.com>
2023-01-04 13:28:26 +0800

    fix(highlight-ssr): migrate to rehype-highlight@6

--- packages/plugin-highlight-ssr/src/index.ts
+++ packages/plugin-highlight-ssr/src/index.ts
@@ -2,12 +2,11 @@ import type { BytemdPlugin } from 'bytemd'
 import rehypeHighlight, { Options } from 'rehype-highlight'
 
 export default function highlightSsr({
-  subset = false,
   ignoreMissing = true,
   ...rest
 }: Options = {}): BytemdPlugin {
   return {
     rehype: (processor) =>
-      processor.use(rehypeHighlight, { subset, ignoreMissing, ...rest }),
+      processor.use(rehypeHighlight, { ignoreMissing, ...rest }),
   }
 }
