e517f6c16c85febc6691579daedc02ee04a66aad
Rongjian Zhang <pd4d10@gmail.com>
2023-10-04 03:00:25 +0800

    refactor: set up extensions

--- packages/bytemd/src/codemirror.ts
+++ packages/bytemd/src/codemirror.ts
@@ -1,5 +1,32 @@
+import {
+  closeBrackets,
+  autocompletion,
+  closeBracketsKeymap,
+  completionKeymap,
+} from '@codemirror/autocomplete'
+import { history, defaultKeymap, historyKeymap } from '@codemirror/commands'
 import { markdown } from '@codemirror/lang-markdown'
-import { EditorView, minimalSetup, basicSetup } from 'codemirror'
+import {
+  foldGutter,
+  indentOnInput,
+  syntaxHighlighting,
+  defaultHighlightStyle,
+  bracketMatching,
+  foldKeymap,
+} from '@codemirror/language'
+import { highlightSelectionMatches, searchKeymap } from '@codemirror/search'
+import {
+  lineNumbers,
+  highlightActiveLineGutter,
+  highlightSpecialChars,
+  drawSelection,
+  dropCursor,
+  rectangularSelection,
+  crosshairCursor,
+  highlightActiveLine,
+  keymap,
+  EditorView,
+} from '@codemirror/view'
 import { LitElement, css } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
 
@@ -22,14 +49,43 @@ export class Codemirror extends LitElement {
       '& .cm-scroller': {
         fontFamily: 'SF Mono, Consolas, Liberation Mono, Menlo, monospace',
         fontSize: '14px',
+        padding: '16px',
       },
     })
 
     this.editor = new EditorView({
       doc: this.value,
       extensions: [
+        // tweaked from basicSetup: https://github.com/codemirror/basic-setup/blob/b3be7cd30496ee578005bd11b1fa6a8b21fcbece/src/codemirror.ts#L50
+        // lineNumbers(),
+        // highlightActiveLineGutter(),
+        highlightSpecialChars(),
+        history(),
+        // foldGutter(),
+        drawSelection(),
+        dropCursor(),
+        // EditorState.allowMultipleSelections.of(true),
+        indentOnInput(),
+        syntaxHighlighting(defaultHighlightStyle, { fallback: true }),
+        bracketMatching(),
+        closeBrackets(),
+        // autocompletion(),
+        rectangularSelection(),
+        crosshairCursor(),
+        // highlightActiveLine(),
+        highlightSelectionMatches(),
+        keymap.of([
+          ...closeBracketsKeymap,
+          ...defaultKeymap,
+          ...searchKeymap,
+          ...historyKeymap,
+          // ...foldKeymap,
+          // ...completionKeymap,
+          // ...lintKeymap,
+        ]),
+
+        // extra
         EditorView.lineWrapping,
-        basicSetup,
         theme,
         markdown({
           extensions: [],
