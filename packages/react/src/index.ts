6a09528975e3da0c1a7c84b3a2e76aff2e6c6e22
Rongjian Zhang <pd4d10@gmail.com>
2023-10-05 00:57:49 +0800

    feat: react wrapper

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,4 +1,21 @@
-export { Editor } from './editor'
-export type { EditorProps } from './editor'
-export { Viewer } from './viewer'
-export type { ViewerProps } from './viewer'
+import { createComponent } from '@lit-labs/react'
+import * as bytemd from 'bytemd'
+import React from 'react'
+
+export const Editor = createComponent({
+  tagName: 'bytemd-editor',
+  elementClass: bytemd.Editor,
+  react: React,
+  events: {
+    onChange: 'change',
+  },
+})
+
+export const Viewer = createComponent({
+  tagName: 'bytemd-viewer',
+  elementClass: bytemd.Viewer,
+  react: React,
+  events: {
+    onMeta: 'meta',
+  },
+})
